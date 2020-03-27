ddd89ed5730d5471151ab9737372b746f3499c68
Pavel <impetus@yandex.ru>
2020-03-27 10:30:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private SharedPreferences shared;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+
+        getSettings();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                shared = getSharedPreferences("base", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = shared.edit();
+                editor.putInt("value",seekBar.getProgress());
+                editor.apply();
+                Snackbar.make(findViewById(R.id.root),R.string.saved,Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void getSettings() {
+        shared = getSharedPreferences("base",Context.MODE_PRIVATE);
+        if(shared != null) {
+            int value = shared.getInt("value",1);
+            seekBar.setProgress(value);
+        }
+    }
+}
